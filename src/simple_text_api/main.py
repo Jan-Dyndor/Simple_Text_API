77d885f0efcd3cb22b3a99c4548d04663e8e5253
Jan Dyndor <jandyndor@gmail.com>
2026-01-04 16:14:12 +0100

    working api endpoints + tests

--- src/simple_text_api/main.py
+++ src/simple_text_api/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from simple_text_api.services.clean_text import clean_input
+from simple_text_api.services.text_analysis import (
+    count_sentences,
+    count_words,
+    most_frequent_char,
+    most_frequent_words,
+)
+from simple_text_api.schemas.schemas import CleanRequest, CleanResponse, AnalyzeResponse
 
 app = FastAPI()
 
@@ -6,3 +14,26 @@ app = FastAPI()
 @app.get("/health")
 def health_check() -> dict:
     return {"Status": "OK"}
+
+
+@app.post("/clean_text", response_model=CleanResponse)
+def clean_text(text: CleanRequest) -> CleanResponse:
+    clean = clean_input(text.input_string)
+    return CleanResponse(clean_text=clean)
+
+
+@app.post("/analyze", response_model=AnalyzeResponse)
+def analyze_text(text: CleanRequest) -> AnalyzeResponse:
+    clean_str = clean_input(text.input_string)
+    words_count = count_words(clean_str)
+    sentence_count = count_sentences(clean_str)
+    frequent_words = most_frequent_words(clean_str)
+    frequent_chars = most_frequent_char(clean_str)
+    return AnalyzeResponse(
+        words_count=words_count,
+        sentence_count=sentence_count,
+        frequent_words=frequent_words,
+        frequent_chars=frequent_chars,
+        orginal_text=text.input_string,
+        clean_text=clean_str,
+    )
---
+++ src/simple_text_api/schemas/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+
+
+class CleanRequest(BaseModel):
+    input_string: str = Field(
+        description="User string to be cleaned", min_length=1, max_length=1000
+    )
+
+
+class CleanResponse(BaseModel):
+    clean_text: str
+
+
+class AnalyzeResponse(BaseModel):
+    words_count: int
+    sentence_count: int
+    frequent_words: dict[str, int]
+    frequent_chars: dict[str, int]
+    orginal_text: str
+    clean_text: str
